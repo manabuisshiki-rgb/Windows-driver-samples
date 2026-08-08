8992d394270410563de0c1994373a338f12dbf7d
manabuisshiki-rgb <manabuisshiki-rgb@github.com>
2026-08-09 02:10:31 +0900

    Fix virtual display mode capacity

--- video/IndirectDisplay/IddSampleDriver/Driver.h
+++ video/IndirectDisplay/IddSampleDriver/Driver.h
@@ -39,7 +39,7 @@ namespace Microsoft
         struct IndirectSampleMonitor
         {
             static constexpr size_t szEdidBlock = 128;
-            static constexpr size_t szModeList = 40;
+            static constexpr size_t szModeList = 48;
 
             const BYTE pEdidBlock[szEdidBlock];
             const struct SampleMonitorMode {
